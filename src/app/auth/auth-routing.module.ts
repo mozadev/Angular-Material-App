50c11ff6dce95bb8eb48d9c54e74c978730e06a5
mozac <cesarmozareyes@gmail.com>
2024-11-10 16:40:36 -0500

    Creation of componenets and lazyLoad

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {  RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+
+// localhost:4200/auth/
+const routes : Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes),],
+  exports: [RouterModule,],
+})
+export class AuthRoutingModule { }
---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+
+// localhost:4200/heroes/''
+const routes: Routes = [
+  {
+    path:'',
+    component: LayoutPageComponent,
+    children: [{path: 'new-hero', component: NewPageComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HeroesRoutingModule { }
